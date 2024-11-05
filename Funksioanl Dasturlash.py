8376c897653b3cb782e98957d19840b00e789edc
Nikotin <developerfoziljon@gmail.com>
2024-11-05 11:17:18 +0500

    05.11.2024

---
+++ Funksioanl Dasturlash.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+
+def main():
+    x = int(input("Ikki sonni kiriting: "))
+    y = int(input())
+
+    result = add(x, y)
+    print("Yig'indi:", result)
+
+if __name__ == "__main__":
+    main()
