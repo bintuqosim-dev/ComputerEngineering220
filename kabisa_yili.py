72a3022168c4538d2e3a4ff1cd87eb6e91ae7a27
Javlonbek <164559793+murodvc@users.noreply.github.com>
2024-11-05 10:53:11 +0500

    Update kabisa_yili.py

--- kabisa_yili.py
+++ kabisa_yili.py
@@ -1,12 +1,7 @@
 def is_kabisa_yili(yil):
-    if (yil % 4 == 0 and yil % 100 != 0) or (yil % 400 == 0):
-        return True
-    else:
-        return False
+    return (yil % 4 == 0 and yil % 100 != 0) or (yil % 400 == 0)
 
 
 yil = int(input("Yilni kiriting: "))
-if is_kabisa_yili(yil):
-    print(f"{yil} kabisa yili.")
-else:
-    print(f"{yil} kabisa yili emas.")
+print(f"{yil} kabisa yili." if is_kabisa_yili(yil) else f"{yil} kabisa yili emas.")
+
